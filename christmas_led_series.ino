b99b7a0fabab09673b6723c72dedf6b7d8a41bcd
deimoshall <deimoshall@gmail.com>
2022-10-31 19:21:49 -0600

    Three modes works with different effects

--- christmas_led_series.ino
+++ christmas_led_series.ino
@@ -1,10 +1,3 @@
-// Program behavior
-//  in1 in2 led
-//   0   0   1
-//   0   1   2
-//   1   0   3
-//   1   1   4
-
 #define INPUT_1 25
 #define INPUT_2 26
 #define LED_1 33
@@ -27,30 +20,101 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("Mode: " + String(getMode()));
+  Serial.println("Mode: " + String(getMode()) + ", Serie: " + String(serie));
+  if (getMode() != 0) {
+    serie = getMode();
+  }
   
-  if (getMode() == 1) {
+  if (serie == 1) {
     mode1();
-  } else if (getMode() == 2) {
+  } else if (serie == 2) {
     mode2();
-  } else if (getMode() == 3) {
+  } else if (serie == 3) {
     mode3();
   }
 }
 
 int getMode() {
   if (digitalRead(INPUT_1) == 0 && digitalRead(INPUT_2) == 0) {
-    return 1;
+    return 0;
   } else if (digitalRead(INPUT_1) == 0 && digitalRead(INPUT_2) == 1) {
-    return 2; 
+    return 1;
   } else if (digitalRead(INPUT_1) == 1 && digitalRead(INPUT_2) == 0) {
-    return 3;
+    return 2;
   } else if (digitalRead(INPUT_1) == 1 && digitalRead(INPUT_2) == 1) {
-    return 4;
+    return 3;
   }
 }
 
 void mode1() {
+  delay(100);
+  
+  if (flag == 0) {
+    flag = 1;
+    
+    digitalWrite(LED_1, LOW);
+    digitalWrite(LED_2, LOW);
+    digitalWrite(LED_3, LOW);
+    digitalWrite(LED_4, HIGH);
+  } else if (flag == 1) {
+    flag = 2;
+    
+    digitalWrite(LED_1, LOW);
+    digitalWrite(LED_2, LOW);
+    digitalWrite(LED_3, HIGH);
+    digitalWrite(LED_4, LOW);
+  } else if (flag == 2) {
+    flag = 3;
+    
+    digitalWrite(LED_1, LOW);
+    digitalWrite(LED_2, HIGH);
+    digitalWrite(LED_3, LOW);
+    digitalWrite(LED_4, LOW);
+  } else {
+    flag = 0;
+    
+    digitalWrite(LED_1, HIGH);
+    digitalWrite(LED_2, LOW);
+    digitalWrite(LED_3, LOW);
+    digitalWrite(LED_4, LOW);
+  }
+}
+
+void mode2() {
+  delay(100);
+  
+  if (flag == 0) {
+    flag = 1;
+    
+    digitalWrite(LED_1, HIGH);
+    digitalWrite(LED_2, LOW);
+    digitalWrite(LED_3, LOW);
+    digitalWrite(LED_4, LOW);
+  } else if (flag == 1) {
+    flag = 2;
+    
+    digitalWrite(LED_1, LOW);
+    digitalWrite(LED_2, HIGH);
+    digitalWrite(LED_3, LOW);
+    digitalWrite(LED_4, LOW);
+  } else if (flag == 2) {
+    flag = 3;
+    
+    digitalWrite(LED_1, LOW);
+    digitalWrite(LED_2, LOW);
+    digitalWrite(LED_3, HIGH);
+    digitalWrite(LED_4, LOW);
+  } else {
+    flag = 0;
+    
+    digitalWrite(LED_1, LOW);
+    digitalWrite(LED_2, LOW);
+    digitalWrite(LED_3, LOW);
+    digitalWrite(LED_4, HIGH);
+  }
+}
+
+void mode3() {
   delay(1000);
   
   if (flag == 0) {
@@ -68,25 +132,4 @@ void mode1() {
     digitalWrite(LED_3, LOW);
     digitalWrite(LED_4, LOW);
   }
-}
-
-void mode2() {
-  digitalWrite(LED_1, LOW);
-  digitalWrite(LED_2, HIGH);
-  digitalWrite(LED_3, LOW);
-  digitalWrite(LED_4, LOW);
-}
-
-void mode3() {
-  digitalWrite(LED_1, LOW);
-  digitalWrite(LED_2, LOW);
-  digitalWrite(LED_3, HIGH);
-  digitalWrite(LED_4, LOW);
-}
-
-void turnOnLed4() {
-  digitalWrite(LED_1, LOW);
-  digitalWrite(LED_2, LOW);
-  digitalWrite(LED_3, LOW);
-  digitalWrite(LED_4, HIGH);
 }
